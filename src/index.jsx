c1f216f25043fde90a00cdd118ecd8d7b27ea9d2
jianfangbhbbc <jianfang.bladen-hovell@bbc.co.uk>
2020-09-27 19:41:57 +0100

    added dashed border to the menu

--- src/index.jsx
+++ src/index.jsx
@@ -59,7 +59,12 @@ class Index extends React.Component {
             fontFamily: "Arial",
             textAlign: "center"
         };
-
+        const boxstyle = {
+            fontFamily: "Arial",
+            textAlign: "center",
+            borderStyle: "dashed",
+            borderWidth: "1px"
+        };
         const inputstyle = {
             border: "thin solid green",
             padding: "0.5rem",
@@ -79,44 +84,46 @@ class Index extends React.Component {
                 <p>
                     The flatmates often borrow money from each other. They need an app to log who owes whom and how much.
                 </p>
-                <h3>
-                    <a href="/api/users">Users</a>
-                </h3>
-                <p>
-                    <form onSubmit={this.addSubmitHandler}>
-                        <input
-                            type='text' maxlength="10" size="10" placeholder=" Add User" name="newuser" style={inputstyle}
-                            onChange={this.addChangeHandler}
-                        />
-                    </form>
-                </p>
-                <p>
-                    <form onSubmit={this.searchSubmitHandler}>
-                        <input
-                            type='text' maxlength="10" size="10" placeholder="Search User" style={inputstyle}
-                            onChange={this.searchChangeHandler}
-                        />
-                    </form>
-                </p>
-                <p>
-                    <form onSubmit={this.borrowSubmitHandler}>
-                        <input
-                            type='text' maxlength="8" size="8" placeholder="lender" style={inputstyle}
-                            onChange={this.lenderChangeHandler}
-                        />
-                        <input
-                            type='text' maxlength="8" size="8" placeholder="borrower" style={inputstyle}
-                            onChange={this.borrowerChangeHandler}
-                        />
-                        <input
-                            type='text' maxlength="6" size="6" placeholder="amount" style={inputstyle}
-                            onChange={this.amountChangeHandler}
-                        />
-                        <input
-                            type='submit' value="Borrow" style={submitstyle}
-                        />
-                    </form>
-                </p>
+                <div style={boxstyle}>
+                    <h3>
+                        <a href="/api/users">Users</a>
+                    </h3>
+                    <p>
+                        <form onSubmit={this.addSubmitHandler}>
+                            <input
+                                type='text' maxlength="10" size="10" placeholder=" Add User" name="newuser" style={inputstyle}
+                                onChange={this.addChangeHandler}
+                            />
+                        </form>
+                    </p>
+                    <p>
+                        <form onSubmit={this.searchSubmitHandler}>
+                            <input
+                                type='text' maxlength="10" size="10" placeholder="Search User" style={inputstyle}
+                                onChange={this.searchChangeHandler}
+                            />
+                        </form>
+                    </p>
+                    <p>
+                        <form onSubmit={this.borrowSubmitHandler}>
+                            <input
+                                type='text' maxlength="8" size="8" placeholder="lender" style={inputstyle}
+                                onChange={this.lenderChangeHandler}
+                            />
+                            <input
+                                type='text' maxlength="8" size="8" placeholder="borrower" style={inputstyle}
+                                onChange={this.borrowerChangeHandler}
+                            />
+                            <input
+                                type='text' maxlength="6" size="6" placeholder="amount" style={inputstyle}
+                                onChange={this.amountChangeHandler}
+                            />
+                            <input
+                                type='submit' value="Borrow" style={submitstyle}
+                            />
+                        </form>
+                    </p>
+                </div>
             </div>
         );
     }
